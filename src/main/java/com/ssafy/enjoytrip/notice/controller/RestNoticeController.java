b4ed9d5c06dcf7234d1e1d124d84eacc5835b6fc
maeugel_rosh <maeugel_rosh@naver.com>
2023-05-03 21:04:49 +0900

    feat: RestNoticeController 완성

---
+++ src/main/java/com/ssafy/enjoytrip/notice/controller/RestNoticeController.java
@@ -0,0 +1,100 @@
+package com.ssafy.enjoytrip.notice.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.enjoytrip.enums.LoginConstant;
+import com.ssafy.enjoytrip.notice.dto.NoticeDto;
+import com.ssafy.enjoytrip.notice.service.NoticeService;
+import com.ssafy.enjoytrip.user.dto.UserDto;
+import com.ssafy.enjoytrip.util.PageNavigation;
+
+@RestController
+@RequestMapping("/notice/api")
+public class RestNoticeController {
+	
+	NoticeService noticeService;
+
+	@Autowired
+	public RestNoticeController(NoticeService noticeService) {
+		super();
+		this.noticeService = noticeService;
+	}
+	
+	@GetMapping("")
+	public ResponseEntity<Map<String, Object>> list(String pgno, String key, String word){
+		Map<String, Object> map = new HashMap<String, Object>();
+		List<NoticeDto> noticeList = null;
+		try {
+			noticeList = noticeService.listNotice(pgno, key, word);
+			PageNavigation navigation = noticeService.makePageNavigation(pgno, key, word);
+			map.put("data", noticeList);
+			map.put("page", navigation);
+			map.put("msg", "조회성공");
+			return ResponseEntity.ok(map);
+		} catch (Exception e) {
+			map.put("msg", "조회실패");
+			return null;
+		}
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<String> delete(int noticeNo){
+		try {
+			noticeService.deleteNotice(noticeNo);
+			return ResponseEntity.ok("삭제완료");
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	@GetMapping("/{noticeNo}")
+	public ResponseEntity<NoticeDto> view(int noticeNo){
+		try {
+			noticeService.hitNotice(noticeNo);
+			NoticeDto notice = noticeService.getNoticeByNoticeNo(noticeNo);
+			return ResponseEntity.ok(notice);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	@PostMapping("/write")
+	public ResponseEntity<String> write(String noticeTitle, String noticeContent, HttpSession session){
+		try {
+			UserDto userDto = (UserDto) session.getAttribute(LoginConstant.LOGIN_ATTRIBUTE_NAME.getValue());
+			NoticeDto noticeDto = new NoticeDto();
+	    	noticeDto.setNoticeTitle(noticeTitle);
+	    	noticeDto.setNoticeContent(noticeContent);
+	    	noticeDto.setUserId(userDto.getUserId());
+	    	noticeService.writeNotice(noticeDto);
+	    	return ResponseEntity.ok("글쓰기 완료");
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	@PatchMapping("/modify")
+	public ResponseEntity<String> modify(int noticeNo, String noticeTitle, String noticeContent){
+		try {
+			noticeService.updateNotice(noticeNo, noticeTitle, noticeContent);
+			return ResponseEntity.ok("수정완료");
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+}
