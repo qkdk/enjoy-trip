f55a2c2f765ca6aec4e01c01a3c8cdf48064c084
maeugel_rosh <maeugel_rosh@naver.com>
2023-05-16 22:13:07 +0900

    fix: noticeWrite 수정

--- src/main/java/com/ssafy/enjoytrip/notice/controller/RestNoticeController.java
+++ src/main/java/com/ssafy/enjoytrip/notice/controller/RestNoticeController.java
@@ -26,77 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notice/api")
 public class RestNoticeController {
 
-<<<<<<< HEAD
-	@Autowired
-	public RestNoticeController(NoticeService noticeService) {
-		super();
-		this.noticeService = noticeService;
-	}
-	
-	@GetMapping("")
-	public ResponseEntity<Map<String, Object>> list(String pgno, String key, String word){
-		Map<String, Object> map = new HashMap<String, Object>();
-		List<NoticeDto> noticeList = null;
-		try {
-			noticeList = noticeService.listNotice(pgno, key, word);
-			PageNavigation navigation = noticeService.makePageNavigation(pgno, key, word);
-			map.put("data", noticeList);
-			map.put("page", navigation);
-			map.put("msg", "조회성공");
-			return ResponseEntity.ok(map);
-		} catch (Exception e) {
-			map.put("msg", "조회실패");
-			return null;
-		}
-	}
-	
-	@DeleteMapping("/delete")
-	public ResponseEntity<String> delete(int noticeNo){
-		try {
-			noticeService.deleteNotice(noticeNo);
-			return ResponseEntity.ok("삭제완료");
-		} catch (Exception e) {
-			return null;
-		}
-	}
-	
-	@GetMapping("/{noticeNo}")
-	public ResponseEntity<NoticeDto> view(@PathVariable int noticeNo){
-		try {
-			noticeService.hitNotice(noticeNo);
-			NoticeDto notice = noticeService.getNoticeByNoticeNo(noticeNo);
-			return ResponseEntity.ok(notice);
-		} catch (Exception e) {
-			return null;
-		}
-	}
-	
-	@PostMapping("/write")
-	public ResponseEntity<String> write(@RequestBody NoticeDto noticeDto){
-		try {
-//			UserDto userDto = (UserDto) session.getAttribute(LoginConstant.LOGIN_ATTRIBUTE_NAME.getValue());
-	    	noticeDto.setNoticeTitle(noticeDto.getNoticeTitle());
-	    	noticeDto.setNoticeContent(noticeDto.getNoticeContent());
-	    	noticeDto.setUserId(noticeDto.getUserId());
-	    	noticeService.writeNotice(noticeDto);
-	    	return ResponseEntity.ok("글쓰기 완료");
-		} catch (Exception e) {
-			return null;
-		}
-	}
-	
-	@PutMapping("/modify")
-	public ResponseEntity<String> modify(@RequestBody NoticeDto noticeDto){
-		try {
-			System.out.println(noticeDto.getNoticeTitle());
-			noticeService.updateNotice(noticeDto.getNoticeNo(), noticeDto.getNoticeTitle(), noticeDto.getNoticeContent());
-//			System.out.println(noticeTitle);
-			return ResponseEntity.ok("수정완료");
-		} catch (Exception e) {
-			return null;
-		}
-	}
-=======
     NoticeService noticeService;
 
     @Autowired
@@ -144,14 +73,12 @@ public class RestNoticeController {
     }
 
     @PostMapping("/write")
-    public ResponseEntity<String> write(@RequestBody String noticeTitle, @RequestBody String noticeContent,
-            HttpSession session) {
+    public ResponseEntity<String> write(@RequestBody NoticeDto noticeDto){
         try {
-            UserDto userDto = (UserDto) session.getAttribute(LoginConstant.LOGIN_ATTRIBUTE_NAME.getValue());
-            NoticeDto noticeDto = new NoticeDto();
-            noticeDto.setNoticeTitle(noticeTitle);
-            noticeDto.setNoticeContent(noticeContent);
-            noticeDto.setUserId(userDto.getUserId());
+//			UserDto userDto = (UserDto) session.getAttribute(LoginConstant.LOGIN_ATTRIBUTE_NAME.getValue());
+	    	noticeDto.setNoticeTitle(noticeDto.getNoticeTitle());
+	    	noticeDto.setNoticeContent(noticeDto.getNoticeContent());
+	    	noticeDto.setUserId(noticeDto.getUserId());
             noticeService.writeNotice(noticeDto);
             return ResponseEntity.ok("글쓰기 완료");
         } catch (Exception e) {
@@ -177,7 +104,6 @@ public class RestNoticeController {
                     HttpStatus.OK);
         }
     }
->>>>>>> e737b944320810673e5ed9e9b552a20f02670cc4
 //	@PutMapping("/modify")
 //	public ResponseEntity<String> modify(@RequestBody int noticeNo, @RequestBody String noticeTitle,@RequestBody String noticeContent){
 //		System.out.println(noticeNo+" "+noticeTitle+" "+noticeContent);
