06d2fcdb6a179f59fd7ea4c36fbc6a1ab9c868bb
qkdk <86948395+qkdk@users.noreply.github.com>
2023-05-18 15:31:33 +0900

    refactor: getAtuority 기능 SecurityUtil로 기능 구현

--- src/main/java/com/ssafy/enjoytrip/user/controller/AuthController.java
+++ src/main/java/com/ssafy/enjoytrip/user/controller/AuthController.java
@@ -2,27 +2,20 @@ package com.ssafy.enjoytrip.user.controller;
 
 import com.ssafy.enjoytrip.user.dto.LoginDto;
 import com.ssafy.enjoytrip.user.dto.LoginResponseDto;
-import com.ssafy.enjoytrip.user.service.UserService;
 import com.ssafy.enjoytrip.util.ResponseTemplate;
+import com.ssafy.enjoytrip.util.SecurityUtil;
 import com.ssafy.enjoytrip.util.jwt.JwtFilter;
 import com.ssafy.enjoytrip.util.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Collection;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,21 +38,16 @@ public class AuthController {
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = tokenProvider.createToken(authentication);
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        String ss = authorities.stream().findFirst().get().toString();
-     
-        System.out.println(ss);
-        
+        String auth = SecurityUtil.getAuthority(authentication);
+
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-//        UserResponseDto userResponseDto = null;
-//        userResponseDto.setAccessToken(jwt);
-//        userResponseDto.setUserRole(authorities.getClass().getName());
+
         return new ResponseEntity<>(
                 ResponseTemplate.<LoginResponseDto>builder()
                         .result(true)
                         .msg("로그인에 성공헸습니다")
-                        .data(new LoginResponseDto(jwt, ss, loginDto.getUserId()))
+                        .data(new LoginResponseDto(jwt, auth, loginDto.getUserId()))
                         .build()
                 , httpHeaders, HttpStatus.OK);
     }
--- src/main/java/com/ssafy/enjoytrip/util/SecurityUtil.java
+++ src/main/java/com/ssafy/enjoytrip/util/SecurityUtil.java
@@ -1,9 +1,11 @@
 package com.ssafy.enjoytrip.util;
 
+import java.util.Collection;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -32,5 +34,10 @@ public class SecurityUtil {
         }
 
         return Optional.ofNullable(userId);
+    }
+
+    public static String getAuthority(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        return authorities.stream().findFirst().get().toString();
     }
 }
