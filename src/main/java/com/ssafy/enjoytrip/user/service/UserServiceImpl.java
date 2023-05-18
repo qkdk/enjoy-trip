d22e5425b91d3b5e5edbef0ee7f9f831e0d61ea0
qkdk <86948395+qkdk@users.noreply.github.com>
2023-05-18 13:57:32 +0900

    feat: 이메일 형식 유효성 검사 구현

--- src/main/java/com/ssafy/enjoytrip/user/service/UserServiceImpl.java
+++ src/main/java/com/ssafy/enjoytrip/user/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.ssafy.enjoytrip.user.repository.UserRepository;
 import com.ssafy.enjoytrip.util.SecurityUtil;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,18 @@ public class UserServiceImpl implements UserService {
         if (userRepository.getUserByUserId(joinDto.getUserId()).orElse(null) != null) {
             throw new RuntimeException("이미 가입되어있는 유저입니다.");
         }
+        // 이메일 도메인 유효성 검사
+        if (!isValidEmail(joinDto)) {
+            throw new RuntimeException("이메일 형식이 올바르지 않습니다.");
+        }
         joinDto.setUserPw(passwordEncoder.encode(joinDto.getUserPw()));
         return userRepository.joinUser(joinDto);
+    }
+
+    private static boolean isValidEmail(JoinDto joinDto) {
+        String EMAIL_PATTERN = "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*" + "(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(joinDto.getUserDomain()).matches();
     }
 
     @Override
