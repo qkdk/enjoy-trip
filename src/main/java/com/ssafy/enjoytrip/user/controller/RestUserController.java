6e4c61dd3eef058cc82008d5c483df8d0acc132c
SeungJong-Lee <maeugel_rosh@naver.com>
2023-05-24 16:23:29 +0900

    fix: modify mapping 변경

--- src/main/java/com/ssafy/enjoytrip/user/controller/RestUserController.java
+++ src/main/java/com/ssafy/enjoytrip/user/controller/RestUserController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -93,7 +94,7 @@ public class RestUserController {
                 HttpStatus.OK);
     }
 
-    @PatchMapping("/modify")
+    @PutMapping("/modify")
     public ResponseEntity<ResponseTemplate> modify(@RequestBody ModifyDto modifyDto) {
         userService.modify(modifyDto);
 
